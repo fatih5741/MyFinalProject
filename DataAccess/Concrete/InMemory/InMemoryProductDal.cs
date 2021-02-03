99709aad8b0c6a5eb3a8c7e1cc8c81fd6dc8d927
Fatih Demir <fatih.demir@nisantasi.edu.tr>
2021-02-03 23:47:43 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,70 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _product;
+        public InMemoryProductDal()
+        {
+            //Oracle,Sql Server,Postgres,MongoDb den geliyormuş gibi manul ürün listesi oluşturduk
+            _product = new List<Product> {
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1}
+            };
+        }
+        public void Add(Product product)
+        {
+            _product.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            
+            //LINQ - LANGUAGE INTEGRATED QUERY
+            //Lambda
+            Product productToDelete =  _product.SingleOrDefault(p=>p.ProductId == product.ProductId); //Bir for veya foreach yerine kullanılan Linq kodu
+
+            _product.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _product;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _product.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdiğim ürün Id'sine sahip olan listedeki ürünü Id'sini bul
+            Product productToUpdate = _product.SingleOrDefault(p => p.ProductId == product.ProductId); //Bir for veya foreach yerine kullanılan Linq kodu
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            product.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
